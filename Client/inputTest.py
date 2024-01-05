0fa0a1ad0718ef470bd23f301c9b7aeb530bf53f
ArturRSobol <artur.r.sobol@gmail.com>
2024-01-05 15:22:41 -0800

    added server and client classes

--- Client/inputTest.py
+++ Client/inputTest.py
@@ -1,46 +1,34 @@
 import pygame
 import sys
 pygame.init()
-screen = pygame.display.set_mode((800, 500))
-screen.fill(pygame.Color("blue"))
+import socket 
 
-x = 350 
-y = 200
-player = pygame.Rect(x,y,100,100)
+HOST = "localhost"
+PORT = 5000
 
+BUFFER_SIZE = 1024
+
+clientSocket = socket.socket()
+clientSocket.connect((HOST, PORT))
+
+
+# controller inpuut stuff
 run = True
-
-font_size = 30
-font = pygame.font.SysFont("Futura" , font_size)
-
-def draw_text(text, font, text_col, x, y):
-    img =font.render(text, True, text_col)
-    screen.blit(img, (x, y))
-
-clock = pygame.time.Clock()
-FPS = 60
-
 pygame.joystick.init()
 joysticks = []
 
 while run:
-    clock.tick(FPS)
-    screen.fill(pygame.Color("midnightblue"))
 
     for joystick in joysticks:
-        draw_text(str(joystick.get_name()), font, pygame.Color("white"), 10, 10)
+        #print(str(joystick.get_name()))
 
         horiz_move = joystick.get_axis(2)
         vert_move = joystick.get_axis(3)
         if (abs(horiz_move) > 0.05):
-            x += horiz_move * 5
+            clientSocket.sendall(f"Horiz: {horiz_move}".encode())
         if (abs(vert_move) > 0.05):
-            y += vert_move * 5
-
-
-    player.topleft = (x, y)
-    pygame.draw.rect(screen, pygame.Color("green"), player)
-
+            clientSocket.sendall(f"Vert: {vert_move}".encode())
+    
     for event in pygame.event.get():
 
         if event.type == pygame.JOYDEVICEADDED:
@@ -51,5 +39,3 @@ while run:
 
         if event.type == pygame.QUIT:
             run = False
-
-    pygame.display.update()
---
+++ Server/srever.py
@@ -0,0 +1,20 @@
+import socket
+
+HOST = "localhost"
+PORT = 5000
+
+BUFFER_SIZE = 1024
+
+listenSocket = socket.socket()
+listenSocket.bind((HOST, PORT))
+
+listenSocket.listen()
+print(f"server is listening on port {HOST}:{PORT}")
+
+serverSocket, clientAdress = listenSocket.accept()
+
+print(f"server accepted connection from {clientAdress}")
+
+while True:
+    data = serverSocket.recv(BUFFER_SIZE)
+    print(data)

1245b058a49824cf3e3b96227aa7c90ee090bbd6
ArturRSobol <artur.r.sobol@gmail.com>
2024-01-24 14:16:29 -0800

    ficed bugs

--- Server/server.py
+++ Server/server.py
@@ -20,7 +20,7 @@ print(f"server accepted connection from {clientAdress}")
 while True:
     data = serverSocket.recv(BUFFER_SIZE).decode()
     testArr = data.split(",")
-    if (testArr.len()):
+    if (len(testArr) >= 6):
         data = f"LeftX: {testArr[0]}, LeftY: {testArr[1]}, RightX: {testArr[2]}, RightY: {testArr[3]}, leftTrigger: {testArr[4]}, RightTrigger: {testArr[5]}"
     os.system("cls")
     print(data)

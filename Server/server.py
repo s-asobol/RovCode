51d73854800adb5a67e757ec838a8da35c0b869e
ArturRSobol <artur.r.sobol@gmail.com>
2024-01-30 21:44:34 -0800

    added servo stuff

--- Server/server.py
+++ Server/server.py
@@ -1,6 +1,21 @@
 import socket
 import time
 import os 
+from adafruit_servokit import ServoKit
+
+#Constants
+nbPCAServo=16 
+#Parameters
+MIN_IMP  =[500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500]
+MAX_IMP  =[2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500]
+MIN_ANG  =[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+MAX_ANG  =[180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180]
+#Objects
+pca = ServoKit(channels=16)
+# function init 
+def init():
+    for i in range(nbPCAServo):
+        pca.servo[i].set_pulse_width_range(MIN_IMP[i] , MAX_IMP[i])
 
 HOST = "192.168.1.11"
 PORT = 5000
@@ -17,6 +32,23 @@ serverSocket, clientAdress = listenSocket.accept()
 
 print(f"server accepted connection from {clientAdress}")
 
+
+
+def pcaScenario():
+    """Scenario to test servo"""
+    for i in range(nbPCAServo):
+        for j in range(MIN_ANG[i],MAX_ANG[i],1):
+            print("Send angle {} to Servo {}".format(j,i))
+            pca.servo[i].angle = j
+            time.sleep(0.01)
+        for j in range(MAX_ANG[i],MIN_ANG[i],-1):
+            print("Send angle {} to Servo {}".format(j,i))
+            pca.servo[i].angle = j
+            time.sleep(0.01)
+        pca.servo[i].angle=None #disable channel
+        time.sleep(0.5)
+
+        
 while True:
     data = serverSocket.recv(BUFFER_SIZE).decode()
     testArr = data.split(",")
@@ -24,6 +56,7 @@ while True:
         data = f"LeftX: {testArr[0]}, LeftY: {testArr[1]}, RightX: {testArr[2]}, RightY: {testArr[3]}, leftTrigger: {testArr[4]}, RightTrigger: {testArr[5]}"
     os.system("clear")
     print(data)
+    pcaScenario()
     
     #for s in testArr:
         

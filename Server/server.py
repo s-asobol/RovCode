8866b308f278d0fe43203c4583baf84d2d8f4990
ArturRSobol <artur.r.sobol@gmail.com>
2024-01-31 15:14:01 -0800

    jb

--- Server/server.py
+++ Server/server.py
@@ -64,10 +64,11 @@ while True:
     
 
     # set direction
+    """
     if(float(testArr[0]) <= 0):
         pca.continuous_servo[9].throttle = 1
     else:
         pca.continuous_servo[9].throttle = -1
-
+    """
 
     pca.continuous_servo[8].throttle = float(testArr[0])

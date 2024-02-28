7b2f2d41acab20d2e3f4e0292a6a8ed41baf3133
ArturRSobol <artur.r.sobol@gmail.com>
2024-02-28 13:59:54 -0800

    Added wiring documentation

--- Client/client.py
+++ Client/client.py
@@ -1,60 +1,148 @@
 import pygame
 import sys
-pygame.init()
 import socket 
 import time
+import numpy as np
 
-
+# IP Address and port on Raspberry Pi
 HOST = "192.168.1.11"
 PORT = 5000
 
-BUFFER_SIZE = 1024
+DEADZONE = 0.01
 
+# Column 1: Motor 1 - facing back on the right
+# Column 2: Motor 2 - facing back on the left
+# Column 3: Motor 3 - facing down in the center
+
+# Row 1: X vector (forwards and back)
+# Row 2: Y vector (left and right)
+# Row 3: Z vector (up and down)
+
+# Row 4: X rotation (roll)
+# Row 5: Y rotation (pitch)
+# Row 6: Z rotation (yaw)
+
+PROPORTIONAL_MATRIX =  [[1, 1, 0],
+                        [0, 0, 0],
+                        [0, 0, 1],
+                        [0, 0, 0],
+                        [0, 0, 0],
+                        [1, -1, 0]]
+
+# Pre-process the joystick input to apply a deadzone, as well as determine the direction
+def preProcessJoystick(axis):
+    # Cube the analog axis value, this should allow for better fine motor control
+    axis = axis**3
+
+    # Apply deadzone
+    if abs(axis) < DEADZONE:
+        axis = 0
+
+    return axis
+
+# Pre-process trigger to apply a deadzone, as well as map from range [-1,1] to range [0,1]
+def preProcessTrigger(axis):
+    axis = axis + 1
+    axis = axis / 2
+
+    # Apply deadzone
+    if axis < DEADZONE:
+        axis = 0
+    
+    return axis
+
+# Returns a matrix normalized so the maximum value in the matrix is 1
+def normalizeMatrix(ary):
+    max = 1
+    
+    # Get maximum magnitude in matrix
+    for elem in ary:
+        if abs(elem) > max:
+            max = abs(elem)
+    
+    # Divide each element by the max value
+    return [x / max for x in ary]
+
+# Take the input from the controller and output throttle and sign for each motor
+# LeftY controls forward/backward movement
+# LeftX controls yaw
+# RightY controls up/down movement
+def pufferfishControl(LeftX, LeftY, RightY):
+    # Declaring the numpy array that will be used for the thrust vectors applied to the motors
+    thrustMatrix = np.array(len(PROPORTIONAL_MATRIX[0]))
+
+    # Using list comprehension, multiply selected rows in the proportional matrix by the controller input and sum the vectors togethor
+    thrustMatrix = thrustMatrix + [x * LeftX for x in PROPORTIONAL_MATRIX[5]]       # Yaw
+    thrustMatrix = thrustMatrix + [x * LeftY for x in PROPORTIONAL_MATRIX[0]]       # Forward/backwards
+    thrustMatrix = thrustMatrix + [x * RightY for x in PROPORTIONAL_MATRIX[2]]      # Up/down
+
+    # Normalize the vector
+    thrustMatrix = normalizeMatrix(thrustMatrix)
+
+    # Declare the arrays for extracting the sign and value
+    signArray = []
+    valueArray = []
+
+    # Pull the sign out from the matrix to make the sign array for the motor controller
+    for elem in thrustMatrix:
+        if elem < 0:
+            signArray.append(1)
+        else:
+            signArray.append(0)
+
+    # Take the absolute value of each element to make the value array for the motor controller
+    for elem in thrustMatrix:
+        valueArray.append(abs(elem))
+
+    # Pad the arrays with 0 if there are fewer than 8 motors
+    signArray = np.pad(signArray, (0, 8 - len(signArray)), constant_values = 0)
+    valueArray = np.pad(valueArray, (0, 8 - len(valueArray)), constant_values = 0)
+
+    # Concatenate the sign and value arrays into a string for transmission
+    sendString = ','.join([str(elem) for elem in (signArray.tolist() + valueArray.tolist())])
+    print(sendString)
+    return sendString
+
+
+# Create a socket and connect to the Raspberry Pi
 clientSocket = socket.socket()
 clientSocket.connect((HOST, PORT))
 
-#placeholder for deadzone
-def preProcessJoy(axis):
-    return axis
-
-# controller inpuut stuff
-
-run = True
+# Initialize pygame and the joysticks array
+pygame.init()
 pygame.joystick.init()
 joysticks = []
 
-# waits until joystick is added
-# adds joystic to joystic array when plugged in 
-#
-    
+# Main loop
+run = True
 while run:
+    # Initialize the string that will be sent to the Raspberry Pi
     sendString = None
-    for joystick in joysticks:
-    #read input from controller
-        LeftX = preProcessJoy(joystick.get_axis(0))
-        LeftY = preProcessJoy(joystick.get_axis(1))
-        RightX = preProcessJoy(joystick.get_axis(2))
-        RightY = preProcessJoy(joystick.get_axis(3))
-        LeftTrigger = preProcessJoy(joystick.get_axis(4))
-        RightTrigger = preProcessJoy(joystick.get_axis(5))
-        sendString = f"{LeftX},{LeftY},{RightX},{RightY},{LeftTrigger},{RightTrigger}"
-    #makes sure the string is not null. Can happen on startup
-    if sendString:
-        clientSocket.sendall(sendString.encode())
-    time.sleep(.1)
 
+    # Add joysticks
     for event in pygame.event.get():
-
         if event.type == pygame.JOYDEVICEADDED:
             print(event)
             joy = pygame.joystick.Joystick(event.device_index)
             joysticks.append(joy)
-
-
         if event.type == pygame.QUIT:
-            run = False
+            run = False    
 
+    # For each available joystick (controller)
+    for joystick in joysticks:
+        # Read input from controller
+        LeftX = preProcessJoystick(joystick.get_axis(0))        # Left joystick X
+        LeftY = preProcessJoystick(joystick.get_axis(1))        # Left joystick Y
+        RightX = preProcessJoystick(joystick.get_axis(2))       # Right joystick X
+        RightY = preProcessJoystick(joystick.get_axis(3))       # Right joystick Y
+        LeftTrigger = preProcessTrigger(joystick.get_axis(4))
+        RightTrigger = preProcessTrigger(joystick.get_axis(5))
 
+        # Process the inputs for proportional control
+        sendString = pufferfishControl(LeftX, LeftY, RightY)
+    # Make sure the string is not null, this can happen on startup
+    if sendString:
+        clientSocket.sendall(sendString.encode())
 
-
-    
+    # Sleep for a bit to not overwhelm Raspberry Pi
+    time.sleep(.1)
--- Server/server.py
+++ Server/server.py
@@ -1,103 +1,82 @@
+
 import socket
 import time
 import os 
 from adafruit_servokit import ServoKit
 
-#Constants
-nbPCAServo=16 
-#Parameters
+NUM_MOTORS=16 
+
+# PWM ranges for each motor
 MIN_IMP  =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-MAX_IMP  =[0,0,0,0,0,0,0,0,6500,6500,0,0,0,0,0,0]
-MIN_ANG  =[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-MAX_ANG  =[180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180]
-#Objects
-pca = ServoKit(channels=16)
-# function init 
-def init():
-    i = 8
-    print("survo inittialized!")
-    pca.continuous_servo[i].set_pulse_width_range(MIN_IMP[i] , MAX_IMP[i])
+MAX_IMP  =[6500,6500,6500,6500,6500,6500,6500,6500,6500,6500,6500,6500,6500,6500,6500,6500]
 
 HOST = "192.168.1.11"
 PORT = 5000
-
 BUFFER_SIZE = 1024
 
+# The index of each motor that gets a PWM signal
+MOTOR_MAPPING   = [0, 2, 4, 6, 8, 10, 12, 14]
+
+# The index of each control bit that gets set high or low
+SIGN_MAPPING    = [1, 3, 5, 7, 9, 11, 13, 15]
+
+pca = ServoKit(channels=16)
+
+# Create a socket and listen for connections
 listenSocket = socket.socket()
 listenSocket.bind((HOST, PORT))
-
 listenSocket.listen()
-print(f"server is listening on port {HOST}:{PORT}")
+print(f"server is listening on {HOST}:{PORT}")
 
+# Accept a connection
 serverSocket, clientAdress = listenSocket.accept()
-
 print(f"server accepted connection from {clientAdress}")
 
+# Initializes the PWM range for each motor
+def init():
+    i = 8
+    pca.continuous_servo[i].set_pulse_width_range(MIN_IMP[i] , MAX_IMP[i])
 
-
-def pcaScenario():
-    """Scenario to test servo"""
-    for i in range(nbPCAServo):
-        for j in range(MIN_ANG[i],MAX_ANG[i],1):
-            print("Send angle {} to Servo {}".format(j,i))
-            pca.continuous_servo[i].angle = j
-            time.sleep(0.01)
-        for j in range(MAX_ANG[i],MIN_ANG[i],-1):
-            print("Send angle {} to Servo {}".format(j,i))
-            pca.continuous_servo[i].angle = j
-            time.sleep(0.01)
-        pca.servo[i].angle=None #disable channel
-        time.sleep(0.5)
-
-def preProcessJoy(axis):
-    sign = 0
-    if axis < 0:
-        sign = 1
-
-    print("axs = " + str(axis))
-    axis = axis**3
-    axis = abs(axis)
-    print("axs = " + str(axis))
-    OldRange = (1 - 0)  
-    NewRange = (2)  
-    NewValue = (((axis) * NewRange) / OldRange) + (-1)
-    return sign, NewValue
-
-#setup
 init()
 
-
+print("before loop")
+# Main loop
 while True:
+    print("in loop")
+    # Receive a packet of data and decode it to a string
     data = serverSocket.recv(BUFFER_SIZE).decode()
+    print("recieved")
+    # Split the string
     testArr = data.split(",")
-    if (len(testArr) >= 6):
-        data = f"LeftX: {testArr[0]}, LeftY: {testArr[1]}, RightX: {testArr[2]}, RightY: {testArr[3]}, leftTrigger: {testArr[4]}, RightTrigger: {testArr[5]}"
+
+    # Slice the array into sign and value
+    signArray = testArr[:8]
+    valueArray = testArr[-8:]
+
+    # Commented this out/didn't implement since printing tends to use a lot of CPU
+    # Print the received values to the screen
+    # os.system("clear")
+    # print(data)
+
+    # Cast signArray values to int
+    for i in range(len(signArray)):
+        signArray[i] = int(signArray[i])
+
+    # Cast valueArray values to float
+    for i in range(len(valueArray)):
+        valueArray[i] = float(valueArray[i])
+
     os.system("clear")
-    #cast array vals to float
-    for i in range(len(testArr)):
-        testArr[i] = float(testArr[i])
-        
-    print(type(testArr[0]))
-    print(testArr[0])
-    print(data)
-    #pcaScenario()
-    
+    print(valueArray)
+    print(signArray)
 
-    # set direction
-    
-    if(float(testArr[0]) <= 0):
-        pca.continuous_servo[9].throttle = 1
-        print("Right")
-    else:
-        pca.continuous_servo[9].throttle = -1
-        print("left")
-    
-    if (testArr[0] < float(0.01)):
-        testArr[0] = -1
-    
-    throttle = preProcessJoy(testArr[0])
-    print(throttle)
+    # Loop through the array and apply the input to the motor
+    for i in range(8):
+        # Set the motor's throttle
+        pca.continuous_servo[MOTOR_MAPPING[i]].throttle = valueArray[i]
 
-
-    pca.continuous_servo[8].throttle = throttle
-    
+        # Set the motor's direction bit based off the sign
+        if(signArray[i] <= 0):
+            pca.continuous_servo[SIGN_MAPPING[i]].throttle = 1
+        else:
+            pca.continuous_servo[SIGN_MAPPING[i]].throttle = -1
